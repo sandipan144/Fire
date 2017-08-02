3533c74c026194ac3753dce46e0e42fd0d59bde5
sandipan144 <sandipanchakraborty144@gmail.com>
2017-08-03 04:11:42 +0530

    2

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function signup() {
-    console.log("signup");
+    var name2 = document.getElementById("name2");
+    var email2 = document.getElementById("email2");
+    var password2 = document.getElementById("password2");
+    var password2_2 = document.getElementById("password2_2");
+    var contact2 = document.getElementById("contact2");
+
+//    console.log(name2.value);
+//    console.log(email2.value);
+//    console.log(password2.value);
+//    console.log(password2_2.value);
+//    console.log(contact2.value);
 }
 
 function login() {
-    console.log("login");
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+//    console.log(email.value);
+//    console.log(password.value);
 }
